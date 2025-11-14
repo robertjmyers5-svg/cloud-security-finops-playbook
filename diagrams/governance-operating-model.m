8e8f76ad42726c2d72c18f83315cf6626496f305
Robert Myers <robert.j.myers5@gmail.com>
2025-11-14 10:43:30 -0600

    Create governance-operating-model.m

---
+++ diagrams/governance-operating-model.m
@@ -0,0 +1,39 @@
+# Cloud Governance Operating Model
+
+```text
++--------------------------------+
+|    Executive Leadership        |
+| - Strategy / Risk Appetite     |
++----------------+---------------+
+                 |
+                 v
++--------------------------------+
+|   Cloud Governance Board       |
+| - Policies / Standards         |
+| - Approved Services            |
+| - Exceptions Process           |
++----------------+---------------+
+                 |
+                 v
++--------------------------------+
+|   Implementation Teams         |
+| - Security / IAM               |
+| - FinOps / Cost                |
+| - Cloud Ops / Platform         |
++----------------+---------------+
+                 |
+                 v
++--------------------------------+
+|  Automation & Guardrails       |
+| - Azure Policy / AWS SCPs      |
+| - Config / Defender / GuardDuty|
+| - CI/CD Enforcement            |
++----------------+---------------+
+                 |
+                 v
++--------------------------------+
+| Monitoring & Reporting         |
+| - Dashboards                   |
+| - Compliance                   |
+| - Cost Optimization            |
++--------------------------------+
